21ee24aba7633d4e11293d86b7b3b3cfc9b01424
Nikolas Freitas <nikolasbvf@gmail.com>
2022-04-13 23:34:59 -0300

    Add more poses into hand (#6)
    * Add more poses into hand
    
    * implementing some new poses
    
    Co-authored-by: Giovani <giovani.boff@uhuu.com>

--- src/module/hand-pose/hand-pose.service.ts
+++ src/module/hand-pose/hand-pose.service.ts
@@ -48,7 +48,171 @@ export class HandPoseService {
         this.fingers.get('pinky')?.contract(),
       ]);
 
-      this.logger.log('like pose executed');
+      this.logger.log('victory pose executed');
+    } catch (error) {
+      this.logger.error(error);
+      throw new Error(error);
+    }
+  }
+
+  @MovingVerification
+  async middleFinger() {
+    try {
+      await Promise.all([
+        await this.fingers.get('middle')?.extend(),
+        this.fingers.get('pointer')?.contract(),
+        this.fingers.get('thumb')?.contract(),
+        this.fingers.get('ring')?.contract(),
+        this.fingers.get('pinky')?.contract(),
+      ]);
+
+      this.logger.log('middle finger pose executed');
+    } catch (error) {
+      this.logger.error(error);
+      throw new Error(error);
+    }
+  }
+
+  @MovingVerification
+  async rock() {
+    try {
+      await Promise.all([
+        this.fingers.get('middle').toPosition(100),
+        await this.fingers.get('thumb')?.contract(),
+        this.fingers.get('pointer')?.extend(),
+        this.fingers.get('middle')?.contract(),
+        this.fingers.get('ring')?.contract(),
+        this.fingers.get('pinky')?.extend(),
+      ]);
+
+      this.logger.log('rock pose executed');
+    } catch (error) {
+      this.logger.error(error);
+      throw new Error(error);
+    }
+  }
+
+  @MovingVerification
+  async hangLoose() {
+    try {
+      await Promise.all([
+        this.fingers.get('middle').toPosition(100),
+        await this.fingers.get('thumb')?.extend(),
+        this.fingers.get('pointer')?.contract(),
+        this.fingers.get('middle')?.contract(),
+        this.fingers.get('ring')?.contract(),
+        this.fingers.get('pinky')?.extend(),
+      ]);
+
+      this.logger.log('hangLoose pose executed');
+    } catch (error) {
+      this.logger.error(error);
+      throw new Error(error);
+    }
+  }
+
+  @MovingVerification
+  async one() {
+    try {
+      await Promise.all([
+        this.fingers.get('thumb')?.contract(),
+        await this.fingers.get('ring')?.contract(),
+        this.fingers.get('pointer')?.extend(),
+        this.fingers.get('middle')?.contract(),
+        this.fingers.get('pinky')?.contract(),
+      ]);
+
+      this.logger.log('one pose executed');
+    } catch (error) {
+      this.logger.error(error);
+      throw new Error(error);
+    }
+  }
+
+  @MovingVerification
+  async three() {
+    try {
+      await Promise.all([
+        this.fingers.get('thumb')?.contract(),
+        await this.fingers.get('ring')?.extend(),
+        this.fingers.get('pointer')?.extend(),
+        this.fingers.get('middle')?.extend(),
+        this.fingers.get('pinky')?.contract(),
+      ]);
+
+      this.logger.log('three pose executed');
+    } catch (error) {
+      this.logger.error(error);
+      throw new Error(error);
+    }
+  }
+
+  @MovingVerification
+  async four() {
+    try {
+      await Promise.all([
+        this.fingers.get('thumb')?.contract(),
+        await this.fingers.get('ring')?.extend(),
+        this.fingers.get('pointer')?.extend(),
+        this.fingers.get('middle')?.extend(),
+        this.fingers.get('pinky')?.extend(),
+      ]);
+
+      this.logger.log('four pose executed');
+    } catch (error) {
+      this.logger.error(error);
+      throw new Error(error);
+    }
+  }
+
+  @MovingVerification
+  async five() {
+    try {
+      await Promise.all([
+        this.fingers.get('thumb')?.extend(),
+        await this.fingers.get('ring')?.extend(),
+        this.fingers.get('pointer')?.extend(),
+        this.fingers.get('middle')?.extend(),
+        this.fingers.get('pinky')?.extend(),
+      ]);
+
+      this.logger.log('five pose executed');
+    } catch (error) {
+      this.logger.error(error);
+      throw new Error(error);
+    }
+  }
+
+  @MovingVerification
+  async zero() {
+    try {
+      await Promise.all([
+        this.fingers.get('thumb')?.contract(),
+        await this.fingers.get('ring')?.contract(),
+        this.fingers.get('pointer')?.contract(),
+        this.fingers.get('middle')?.contract(),
+        this.fingers.get('pinky')?.contract(),
+      ]);
+
+      this.logger.log('zero pose executed');
+    } catch (error) {
+      this.logger.error(error);
+      throw new Error(error);
+    }
+  }
+
+  @MovingVerification
+  async dusGuri() {
+    try {
+      await Promise.all([
+        this.fingers.get('thumb')?.contract(),
+        await this.fingers.get('ring')?.contract(),
+        this.fingers.get('pointer')?.extend(),
+        this.fingers.get('middle')?.extend(),
+        this.fingers.get('pinky')?.extend(),
+      ]);
+
+      this.logger.log('dusguri pose executed');
     } catch (error) {
       this.logger.error(error);
       throw new Error(error);
